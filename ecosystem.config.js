360bf05a678c404c003de40f526a075eaeed9476
desarrollomeetcce <80836403+desarrollomeetcce@users.noreply.github.com>
2026-01-05 13:00:04 -0600

    Ecosystem para prod corregido

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: "multiagente-flow", // cámbialo si quieres
+      name: "multiagente-flow",
+
+      // binario real de Next
       script: "node_modules/next/dist/bin/next",
+
+      // argumentos
       args: "start -p 3002",
-      cwd: "/var/www/multiagente", // 🔴 ruta absoluta del proyecto
+
+      // 👉 cwd relativo (opcional pero recomendable)
+      cwd: ".",
+
       exec_mode: "fork",
-      instances: 1, // Next maneja el render, no cluster aquí
+      instances: 1,
+
       autorestart: true,
       watch: false,
 
@@ -17,8 +25,8 @@ module.exports = {
 
       max_memory_restart: "800M",
 
-      error_file: "./logs/pm2-error.log",
-      out_file: "./logs/pm2-out.log",
+      error_file: "logs/pm2-error.log",
+      out_file: "logs/pm2-out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
     },
   ],
